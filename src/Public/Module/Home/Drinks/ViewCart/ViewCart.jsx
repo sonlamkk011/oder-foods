c428ad404e92bd15a5e6ec858f1c3d8c0ebb52f9
sonla <sonlamkk011@gmail.com>
2022-08-02 23:28:08 +0700

    update

--- src/Public/Module/Home/Drinks/ViewCart/ViewCart.jsx
+++ src/Public/Module/Home/Drinks/ViewCart/ViewCart.jsx
@@ -19,21 +19,38 @@ const ViewCart = ({ cart, setCart, handleChange }) => {
     useEffect(() => {
         handlePrice();
     })
-    
-    return(
+
+    return (
         <>
-        <div id="view-cart">
-            <div className="container">
-            <div className="foods">
-            <h1 className="title">View Cart</h1>
-            {cart.map((list) => (
-                <div className="cart-box" key={list.id}>
-                    
+            <div id="view-cart">
+                <div className="container">
+                    <div className="foods">
+                        <h1 className="title">View Cart</h1>
+                        {cart.map((list) => (
+                            <div className="cart-box" key={list.id}>
+                                <div className="cart-img">
+                                    <img src={list.image} alt="" />
+                                    <p>{list.price}</p>
+                                </div>
+                                <div>
+                                    <button onClick={() => handleChange(list, 1)}>+</button>
+                                    <button>{list.amount}</button>
+                                    <button onClick={() => handleChange(list, -1)}>-</button>
+                                </div>
+                                <div>
+                                    <span>{list.price}</span>
+                                    <button onClick={() => handleRemove(list.id)}>Remove</button>
+                                </div>
+
+                            </div>
+                        ))}
+                        <div className="total">
+                            <span>Total Price of your Cart</span>
+                            <span>Rs - {price}</span>
+                        </div>
                     </div>
-            ))}
-          </div>
+                </div>
             </div>
-        </div>
         </>
     )
 }
