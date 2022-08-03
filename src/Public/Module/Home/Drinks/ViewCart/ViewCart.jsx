9ee9935bd5394fcc2e38a1737cb5085cd6d0bf64
Son Lam <sonlamkk011@gmail.com>
2022-08-03 14:31:06 +0700

    update

--- src/Public/Module/Home/Drinks/ViewCart/ViewCart.jsx
+++ src/Public/Module/Home/Drinks/ViewCart/ViewCart.jsx
@@ -2,12 +2,23 @@ import { useState } from "react";
 import { useEffect } from "react";
 import Search from "../../Foods/Search/Search";
 import "./ViewCart.scss";
-import './Cart.css';
+import "./Cart.css";
 import { Button } from "@mui/material";
+import TextField from "@mui/material/TextField";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import { Link } from "react-router-dom";
+import { CartContext } from "../../../Contexts/Cart";
+
+
 
 const ViewCart = ({ cart, setCart, handleChange }) => {
   const [price, setPrice] = useState(0);
-  const [amount, setAmount] = useState(0);
+  const [open, setOpen] = useState(false);
+  const [count, setCount] = useState(1);
 
   const handleRemove = (id) => {
     const arr = cart.filter((item) => item.id !== id);
@@ -16,10 +27,28 @@ const ViewCart = ({ cart, setCart, handleChange }) => {
   };
   const handlePrice = () => {
     let ans = 0;
-    cart.map((item) => (ans += item.amount * item.price));
+    cart.map((item) => (ans += count * item.price));
     setPrice(ans);
   };
-  console.log("asssssssssss", cart);
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+  const handleAddcount =() => {
+    setCount(count +1);
+  }
+  const handleRemoveCount = () => {
+if(count == 0 ){
+
+}else{
+  setCount(count -1)
+}
+
+  }
+
   useEffect(() => {
     handlePrice();
   });
@@ -31,7 +60,7 @@ const ViewCart = ({ cart, setCart, handleChange }) => {
           <div className="foods">
             <h1 className="title"> View Cart</h1>
           </div>
-         
+
           <article>
             {cart.map((item) => (
               <div className="cart_box" key={item.id}>
@@ -40,26 +69,50 @@ const ViewCart = ({ cart, setCart, handleChange }) => {
                   <p className="name">{item.name}</p>
                 </div>
                 <div>
-                  <button onClick={() => handleChange(item, 1)}>+</button>
-                  ({item.amount})
-                  <button onClick={() => handleChange(item, -1)}>-</button>
+                  <button onClick={handleAddcount}>+</button>
+                  ({count})
+                  
+                  <button onClick={handleRemoveCount}>-</button>
                 </div>
                 <div>
                   <span>{item.price} VND</span>
-                  <button onClick={() => handleRemove(item.id)}>Remove</button>
+                  <button onClick={() => handleRemove(item.id)}>
+                    Remove 
+                    </button>
                 </div>
               </div>
             ))}
             <div className="total">
               <span>Total Price </span>
-              <span>Rs - {price}</span>
-              <div className="button">
-            <Button>Oder</Button>
-          </div>
+              <span>{price} VND</span>
+              <div className="button" onClick={handleClickOpen}>
+                <Button className="oder">Oder</Button>
+                <div>
+
+                <Dialog open={open} onClose={handleClose} >
+                  <DialogTitle >Thêm ghi chú cho món ăn : </DialogTitle>
+                  <DialogContent>
+                    <TextField
+                      autoFocus
+                      margin="dense"
+                      id="name"
+                      label="Thêm Ghi Chú ..."
+                      type="text"
+                      fullWidth
+                      variant="standard"
+                    />
+                  </DialogContent>
+                  <DialogActions>
+                    <Button onClick={handleClose}>Cancel</Button>
+                    <Link to="/oder-details">
+                    <Button onClick={handleClose}>Oder</Button>
+                    </Link>
+                  </DialogActions>
+                </Dialog>
+                </div>
+              </div>
             </div>
-            
           </article>
-          
         </div>
       </div>
     </>
